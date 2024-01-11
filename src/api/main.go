049440b70cdb1275cc93a9f323d2fdea20330ec3
Niels <noleu66@posteo.net>
2024-01-11 21:06:46 +0100

    removed deprecated code

--- src/api/main.go
+++ src/api/main.go
@@ -6,8 +6,8 @@ import (
 	"gorm.io/gorm"
 	"log"
 	"net/http"
-	"os"
 	"software_containerization/Models"
+	"time"
 )
 
 func main() {
@@ -15,36 +15,35 @@ func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
 	log.Println("starting sever")
+	database_int()
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		_, _ = fmt.Fprintln(w, "Hello World")
 	})
 	log.Fatal(http.ListenAndServe(":8080", nil))
-	database_int()
 
 }
 
 // func database_int() gorm.DB { // not sure if needed
 func database_int() {
-	db_url := os.Getenv("DATABASE_URL")
-	if db_url == "" {
-		log.Fatal("DATABASE_URL environment variable not set")
-		os.Exit(-1)
-	}
-	dsn := "host=database user=postgres password=postgres dbname=Events port=9920 sslmode=disable TimeZone=Europe/Berlin"
+	log.Println("connecting to database")
+	log.Println("waiting for database to start")
+	time.Sleep(time.Duration(15) * time.Second)
+
+	dsn := "host=database user=postgres password=postgres dbname=Events port=5432 sslmode=disable TimeZone=Europe/Berlin"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err)
-	} else {
-		log.Println("Database connected")
 	}
 
+	log.Println("connection to database established")
+	log.Println("migrating database")
 	err = db.AutoMigrate(&Models.Event{}, &Models.User{}, &Models.Role{})
 
 	if err == nil {
 		log.Fatal(err)
 	}
-
+	log.Println("database migration finished")
 	//return gorm.DB{}
 	//return *db // not sure pointer arithmetics
 }
