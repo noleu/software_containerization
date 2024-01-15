86ea00089419f8eb96787b4ae0e20de3747e3479
Qi-Mo Lin <qimolin9@gmail.com>
2024-01-15 12:45:56 +0100

    add docker configuration for frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,9 +5,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  server: {
+    host: '0.0.0.0'
+  },
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
