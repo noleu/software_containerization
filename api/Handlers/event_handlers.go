595210e2363a584faba3635ce26008884e5d65b4
RawAlsaadi <raw1.alsaadi@gmail.com>
2024-01-18 20:27:28 +0100

    Add routes

---
+++ api/Handlers/event_handlers.go
@@ -0,0 +1,120 @@
+package Handlers
+
+import (
+	"net/http"
+	"software_containerization/Models"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+	"errors"
+)
+
+func CreateEventHandler(db *gorm.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var newEvent Models.Event
+
+		// Bind the JSON data to newEvent
+		if err := c.ShouldBindJSON(&newEvent); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		// Validate that the Organizer exists
+		var organizer Models.User
+		if err := db.First(&organizer, newEvent.OrganizerId).Error; err != nil {
+			if errors.Is(err, gorm.ErrRecordNotFound) {
+				c.JSON(http.StatusBadRequest, gin.H{"error": "Organizer not found"})
+				return
+			} else {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				return
+			}
+		}
+
+		// Create the event if the organizer is valid
+		if result := db.Create(&newEvent); result.Error != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
+			return
+		}
+		c.JSON(http.StatusCreated, newEvent)
+	}
+}
+
+func GetEventsHandler(db *gorm.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var events []Models.Event
+		if result := db.Preload("Organizer").Find(&events); result.Error != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, events)
+	}
+}
+
+func GetEventByIdHandler(db *gorm.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var event Models.Event
+		eventId := c.Param("id")
+
+		if result := db.First(&event, eventId); result.Error != nil {
+			if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+				c.JSON(http.StatusNotFound, gin.H{"error": "Event not found"})
+			} else {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
+			}
+			return
+		}
+		c.JSON(http.StatusOK, event)
+	}
+}
+
+func GetEventsByUserIdHandler(db *gorm.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var events []Models.Event
+		userId := c.Param("userId")
+
+		if result := db.Where("organizer_id = ?", userId).Find(&events); result.Error != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, events)
+	}
+}
+
+func DeleteEventHandler(db *gorm.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		eventId := c.Param("id")
+
+		if result := db.Delete(&Models.Event{}, eventId); result.Error != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"message": "Event deleted"})
+	}
+}
+
+func UpdateEventHandler(db *gorm.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var input Models.UpdateEventInput
+		eventId := c.Param("id")
+
+		// Bind the JSON data to input
+		if err := c.ShouldBindJSON(&input); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		// Find the event to be updated
+		var event Models.Event
+		if result := db.First(&event, eventId); result.Error != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Event not found"})
+			return
+		}
+
+		// Update event in the database
+		if result := db.Model(&event).Updates(input); result.Error != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, event)
+	}
+}
